aad217613be0a37e0c6f0a6a1355ebda11cd7b8e
Rico Zhu <33135503+ricozhuthegreat@users.noreply.github.com>
2019-11-02 14:45:25 -0400

    Updated Firebase
     - Changed code to now deploy via realtime database instead of cloud firestore

--- public/src/js/console.js
+++ public/src/js/console.js
@@ -1,6 +1,9 @@
 
-// Firestore initialization
-let db = firebase.firestore(app);
+// Constant time increment string array for the table
+const GRAPH_LABELS = ["0", "5", "10", "15", "20"];
+
+// Realtime database initialization
+let db = firebase.database();
 
 // Side navbar user informatics/metrics
 let user_greeting = document.getElementById("user-greeting");
@@ -12,6 +15,8 @@ firebase.auth().onAuthStateChanged(function(user) {
 
   // If the current user is verified, continue with webpage updates
   if (user !== null) {
+
+    /* Draw side nav bar info */
 
     // Get the user's email from firebase
     let email = user.email;
@@ -37,6 +42,41 @@ firebase.auth().onAuthStateChanged(function(user) {
     user_profile_pic.appendChild(user_profile_pic_img);
     user_info.appendChild(user_info_el.appendChild(user_info_tn));
 
+
+    /* Fetch data from cloud realtime database and pipe/feed it into the widgets */
+
+
+    /* Build and draw the energy level graph */
+
+    // Draw graph using Chart.js
+    var ctx = document.getElementById('sleep-graph').getContext('2d');
+    var chart = new Chart(ctx, {
+        // The type of chart we want to create
+        type: 'line',
+
+        // The data for our dataset
+        data: {
+            labels: GRAPH_LABELS,
+            datasets: [{
+                label: 'Energy',
+                backgroundColor: '#70ff92',
+                borderColor: '#30ff4c',
+                data: [20, 25, 20, 30, 35, 40, 65]
+            }]
+        },
+
+        // Configuration options go here
+        options: {}
+    });
+
+
   }
 
 });
+
+// Every 5 seconds, call a periodic function that updates the sleep graph/chart
+setInterval(function() {
+
+
+
+}, 5000);
