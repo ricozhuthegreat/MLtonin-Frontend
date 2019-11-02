9394a8e0b337b40cfba718c35b8d399edcb127e5
Rico Zhu <33135503+ricozhuthegreat@users.noreply.github.com>
2019-11-02 11:12:18 -0400

    Updated SideNav
     - Added card design

--- public/src/js/console.js
+++ public/src/js/console.js
@@ -19,14 +19,22 @@ firebase.auth().onAuthStateChanged(function(user) {
     let username = email.substring(0, email.indexOf("@"));
     // Append sample message for greeting. TODO: check time and append differeing texts (e.g., Good evening, Good morning .etc)
     let user_greeting_tn = document.createTextNode("Welcome back " + username);
+    username = "@" + username + "   " + String.fromCodePoint(0x1F971);
     let user_info_tn = document.createTextNode(username);
 
     // Create html elements to append the text nodes
     let user_greeting_el = document.createElement("h1");
     let user_info_el = document.createElement("h2");
 
+    // Create the image for the user profile picture
+    let user_profile_pic_img = document.createElement("img");
+    user_profile_pic_img.setAttribute("src", "../../assets/blank_profile.png");
+    user_profile_pic_img.style.width = "40vh";
+    user_profile_pic_img.style.height = "50vh";
+
     // Append the text nodes to html elements and attach under associated profile section
     user_greeting.appendChild(user_greeting_el.appendChild(user_greeting_tn));
+    user_profile_pic.appendChild(user_profile_pic_img);
     user_info.appendChild(user_info_el.appendChild(user_info_tn));
 
   }
