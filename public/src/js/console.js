8f190ad8b7b94f8996478fb343ca5287a7a2e853
Rico Zhu <33135503+ricozhuthegreat@users.noreply.github.com>
2019-11-02 16:58:29 -0400

    Update console.js
     - Added chart updating code with animations (it looks sooooo clean!)

--- public/src/js/console.js
+++ public/src/js/console.js
@@ -2,6 +2,9 @@
 // Constant time increment string array for the table
 const GRAPH_LABELS = ["0", "5", "10", "15", "20"];
 
+// Integer user energy level array
+let energy_data_array = new Array(5);
+
 // Realtime database initialization
 let db = firebase.database();
 
@@ -9,6 +12,24 @@ let db = firebase.database();
 let user_greeting = document.getElementById("user-greeting");
 let user_profile_pic = document.getElementById("user-profile-pic");
 let user_info = document.getElementById("user-info");
+
+// This function appends data to the chart
+function addData(chart, label, data) {
+    chart.data.labels.push(label);
+    chart.data.datasets.forEach((dataset) => {
+        dataset.data.push(data);
+    });
+    chart.update();
+}
+
+// This function remvoes data from the chart
+function removeData(chart) {
+    chart.data.labels.pop();
+    chart.data.datasets.forEach((dataset) => {
+        dataset.data.pop();
+    });
+    chart.update();
+}
 
 // Event Listener for DOM intro text append
 firebase.auth().onAuthStateChanged(function(user) {
@@ -24,8 +45,7 @@ firebase.auth().onAuthStateChanged(function(user) {
     let username = email.substring(0, email.indexOf("@"));
     // Append sample message for greeting. TODO: check time and append differeing texts (e.g., Good evening, Good morning .etc)
     let user_greeting_tn = document.createTextNode("Welcome back, " + username);
-    username = "@" + username + "   " + String.fromCodePoint(0x1F971);
-    let user_info_tn = document.createTextNode(username);
+    let user_info_tn = document.createTextNode("@" + username + "   " + String.fromCodePoint(0x1F971));
 
     // Create html elements to append the text nodes
     let user_greeting_el = document.createElement("h1");
@@ -43,40 +63,69 @@ firebase.auth().onAuthStateChanged(function(user) {
     user_info.appendChild(user_info_el.appendChild(user_info_tn));
 
 
-    /* Fetch data from cloud realtime database and pipe/feed it into the widgets */
+    /* Fetch data from the realtime database and pipe/feed it into the widgets */
 
+    // Fetch a realtime database reference object
+    let db_ref_object = db.ref().child("users").child(username);
 
-    /* Build and draw the energy level graph */
+    // Retrieve the periodic 'value' updates and add a listener
+    db_ref_object.on("value", function(snapshot) {
 
-    // Draw graph using Chart.js
-    var ctx = document.getElementById('sleep-graph').getContext('2d');
-    var chart = new Chart(ctx, {
-        // The type of chart we want to create
-        type: 'line',
+      // Retrieve the current non-sql data tree (as json)
+      let snap_val = snapshot.val();
+      let snap_val_json_string = JSON.stringify(snap_val, null);
+      // Get the entire array parsed with ("energy":)
+      let snap_val_array = snap_val_json_string.split("\"energy\":");
 
-        // The data for our dataset
-        data: {
-            labels: GRAPH_LABELS,
-            datasets: [{
-                label: 'Energy',
-                backgroundColor: '#70ff92',
-                borderColor: '#30ff4c',
-                data: [20, 25, 20, 30, 35, 40, 65]
-            }]
-        },
+      // Iterate over the last 5 values in the snapshot values string array
+      for (let i=snap_val_array.length-1; i>(snap_val_array.length-6); i--) {
 
-        // Configuration options go here
-        options: {}
+          // Get the first and second characters of the integer (note that index 0 is skipped because it is the head of the tree)
+          let fc = snap_val_array[i].charAt(0);
+          let sc = snap_val_array[i].charAt(1);
+
+          // If the second character is a 0, it is 10 (the only 2 character number accepted)
+          if (sc === "0") {
+            energy_data_array[(snap_val_array.length-1) - i] = parseInt(fc + sc);
+          } else {
+            energy_data_array[(snap_val_array.length-1) - i] = parseInt(fc);
+          }
+
+      }
+
     });
 
+    /* Build and draw the energy level graph */
+    removeData(chart);
 
   }
 
 });
 
+// Draw graph using Chart.js
+var ctx = document.getElementById('sleep-graph').getContext('2d');
+var chart = new Chart(ctx, {
+    // The type of chart we want to create
+    type: 'line',
+
+    // The data for our dataset
+    data: {
+        labels: GRAPH_LABELS,
+        datasets: [{
+            label: 'Energy',
+            backgroundColor: '#70ff92',
+            borderColor: '#30ff4c',
+            data: energy_data_array
+        }]
+    },
+
+    // Configuration options go here
+    options: {}
+});
+
 // Every 5 seconds, call a periodic function that updates the sleep graph/chart
 setInterval(function() {
-
-
-
+  // Remove and re-apply data
+  addData(chart, GRAPH_LABELS, energy_data_array);
+  removeData(chart);
 }, 5000);
