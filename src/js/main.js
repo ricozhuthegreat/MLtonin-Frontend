616f5e6327f2f25d615b5887e2df2ff77cea22a2
Rico Zhu <33135503+ricozhuthegreat@users.noreply.github.com>
2019-11-01 23:45:07 -0400

    Reformatted Directories

---
+++ src/js/main.js
@@ -0,0 +1,5 @@
+
+// Initialize the particles-js configurations for the "floating particles" background
+particlesJS.load('particles-js', '/assets/particles.json', function() {
+  console.log('callback - particles.js config loaded');
+});
