cbd6e5803269e7748925d284fb38faf207424169
Rico Zhu <33135503+ricozhuthegreat@users.noreply.github.com>
2019-11-02 08:00:47 -0400

    Fixed Bug with Firebase Signup
     - Patched a bug where firebase.auth is null

---
+++ public/src/js/firebaseinit.js
@@ -0,0 +1,14 @@
+// Firebase configuration key
+var firebaseConfig = {
+  apiKey: "AIzaSyA6wZISMpsD6dkmiX1R-kdYLeDlaJcz1Ps",
+  authDomain: "electriccityhacks4.firebaseapp.com",
+  databaseURL: "https://electriccityhacks4.firebaseio.com",
+  projectId: "electriccityhacks4",
+  storageBucket: "electriccityhacks4.appspot.com",
+  messagingSenderId: "1043458520100",
+  appId: "1:1043458520100:web:193877abed0f65232614b1",
+  measurementId: "G-T8XSNTMXT1"
+};
+// Initialize the app
+firebase.initializeApp(firebaseConfig);
+console.log("Bruh");
